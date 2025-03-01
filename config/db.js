1af7de85ff613f422508b30c10d18ba1be6578bc
shanni kant <shannimura8683@gmail.com>
2025-03-01 14:26:49 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-    console.log("MONGO_URI:", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB Connected Successfully!");
   } catch (error) {
-    console.error("Database Connection Failed", error);
+    console.error("❌ Database Connection Failed:", error);
     process.exit(1);
   }
 };
